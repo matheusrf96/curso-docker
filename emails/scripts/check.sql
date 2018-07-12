580e91a7330e6f45a43e23c06ae21aaa1c3505d5
Matheus <matheusrf96@gmail.com>
2018-07-12 15:29:38 -0300

    Correção db

--- emails/scripts/check.sql
+++ emails/scripts/check.sql
@@ -1,4 +1,14 @@
 \l
 
 \c email_sender
+
+CREATE TABLE emails (
+    id SERIAL PRIMARY KEY NOT NULL,
+    data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    assunto VARCHAR(100) NOT NULL,
+    mensagem VARCHAR(100) NOT NULL
+);
+
 \d emails
+
+SELECT * FROM emails;
